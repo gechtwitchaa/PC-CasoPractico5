ba6708231006ab006089d6b996728b44a6dde94e
Alberto González Olmedo <146822651+gechtwitchaa@users.noreply.github.com>
2025-12-15 14:11:39 +0100

    Add files via upload

---
+++ wakanda-smart-city/src/main/java/com/ejemplo/wakandasmartcity/WakandaSmartCityApplication.java
@@ -0,0 +1,8 @@
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
+
