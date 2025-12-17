99eb6298c3caf2de314816dcfb97c9d45755b6ea
chahla1 <67165854+chahla1@users.noreply.github.com>
2025-12-17 23:07:04 +0100

    Delete wakanda-smart-city directory

--- wakanda-smart-city/src/main/java/com/ejemplo/wakandasmartcity/WakandaSmartCityApplication.java
+++
@@ -1,8 +0,0 @@
-@EnableEurekaServer
-@SpringBootApplication
-public class EurekaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
-    }
-}
-
